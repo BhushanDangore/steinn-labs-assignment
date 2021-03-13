c544f665fb64e7d4f8a9862a7299bea35650d19f
BhushanDangore <bhushan.dangore@hotmail.com>
2021-03-14 00:27:22 +0530

    env var rename

--- src/helper/environment.js
+++ src/helper/environment.js
@@ -1,5 +1,5 @@
 export function getHostName() {
-  if (process.env.HOST) return process.env.HOST;
+  if (process.env.HOST_NAME) return process.env.HOST_NAME;
   else {
     return "http://localhost:3000";
   }
