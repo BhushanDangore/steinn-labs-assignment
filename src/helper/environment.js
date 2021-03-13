9feaae1d82e632c1f13859ea08847833073aca56
BhushanDangore <bhushan.dangore@hotmail.com>
2021-03-14 00:45:58 +0530

    env fix

--- src/helper/environment.js
+++ src/helper/environment.js
@@ -1,5 +1,4 @@
 export function getHostName() {
-  if (process.env.NODE_ENV === "production")
-    return "https://steinn.herokuapp.com/";
+  if (process.env.REACT_APP_HOST) return process.env.REACT_APP_HOST;
   return "http://localhost:3000";
 }
